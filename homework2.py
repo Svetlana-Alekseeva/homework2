a3152ff9c82dc776fc3f71ea41675737a852d5a3
Svetlana-Alekseeva <sveta-93uzl@mail.ru>
2024-07-02 15:21:51 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+complete_homework = 12
+hours_spent = 1.5
+name_of_course = 'Python'
+time_for_1_task =  hours_spent / complete_homework
+print('Курс: ' + name_of_course, 'всего задач: ' + str(complete_homework,), 'затрачено часов: ' + str(hours_spent), 'среднее время выполнения ' + str(time_for_1_task) + ' часа.', sep = ",")
